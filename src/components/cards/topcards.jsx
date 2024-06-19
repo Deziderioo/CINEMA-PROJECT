55f344e86a425d4ac9815c5bc3f732eca4c26884
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-19 16:32:19 -0300

    responsive added

--- src/components/cards/topcards.jsx
+++ src/components/cards/topcards.jsx
@@ -1,24 +1,39 @@
+import { useEffect, useState } from 'react';
 import style from "./topcards.module.css";
 import { XCircle } from "@phosphor-icons/react";
 import Draggable from 'react-draggable'; 
 
 export default function MovieInfo(props) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // check initial size
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <Draggable>
-            <div className={style.wrap}>
-                <button onClick={props.onClose} className={style.closeButton}>
-                    <XCircle size={24} weight="fill" />
-                </button>
-                <strong>{props.title}</strong>
-                <img src={props.image} alt="" onClick={props.onImageClick} />
-                <ul className={style.sinopse}><p>{props.sinopse}</p></ul>
-                <div>
-                    <p className={style.releaseDateStyle}>Release Date</p>
-                    <p>{props.releaseDate}</p>
+        <>
+            <Draggable disabled={isMobile}>
+                <div className={style.wrap}>
+                    <button onClick={props.onClose} className={style.closeButton}>
+                        <XCircle size={23} weight="fill" />
+                    </button>
+                    <strong>{props.title}</strong>
+                    <img src={props.image} alt="" onClick={props.onImageClick} />
+                    <ul className={style.sinopse}><p>{props.sinopse}</p></ul>
+                    <div>
+                        <p className={style.releaseDateStyle}>Release Date</p>
+                        <p>{props.releaseDate}</p>
+                    </div>
+                    <button className={style.creditsButton}><a href="/credits">credits</a></button>   
                 </div>
-                <button className={style.creditsButton}><a href="/credits">credits</a></button>
-                
-            </div>
-        </Draggable>
+            </Draggable>
+        </>
     );
 }
